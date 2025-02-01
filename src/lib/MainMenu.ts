cb69df2cd91a47d19830345583ea5bab8854d33f
24Fan <120273527+24Fan@users.noreply.github.com>
2025-02-01 16:27:39 -0500

    Implemented menu class and cleaned up sketch.js.

---
+++ src/lib/MainMenu.ts
@@ -0,0 +1,238 @@
+import p5 from 'p5';
+import GameObject from './GameObject';
+
+/*
+Menu Codes:
+0 = Main menu
+1 = World Select
+2 = Difficulty
+3 = Settings
+4 = CUstomization
+99 = Game (no longer in a menu)
+*/
+
+/*
+World Codes:
+0 = Demo
+1 = World 1
+2 = World 2
+3 = World 3
+*/
+
+export default class Menu implements GameObject {
+    private imgLogo!: p5.Image; // The logo will be assigned before use in preload()
+    private MENU = 0;
+    private WORLD = 0;
+    private DIFFICULTY = 0;
+    private clickCooldown = 0; // Click debounce on menus
+
+    constructor(private p: p5) {}
+
+    preload(): void {
+        this.imgLogo = this.p.loadImage('/logo.png');
+    }
+
+    setup(): void {
+        this.p.rectMode(this.p.CENTER);
+    }
+
+    draw(): void {
+        if (this.clickCooldown > 0) {
+            this.clickCooldown--;
+        }
+
+        this.p.background(this.imgLogo);
+
+        switch (this.MENU) {
+            case 0:
+                this.drawMainMenu();
+                break;
+            case 1:
+                this.drawWorldSelect();
+                break;
+            case 2:
+                this.drawDifficultySelect();
+                break;
+            case 3:
+                this.p.background(255, 0, 255);
+                this.p.fill(0);
+                this.p.textSize(30);
+                this.p.text('Settings', this.p.width / 2, 150);
+                break;
+            case 4:
+                this.p.background(255, 0, 0);
+                this.p.fill(255);
+                this.p.textSize(50);
+                this.p.text('Character Customization', this.p.width / 2, this.p.height / 2);
+                break;
+            case 99:
+                // Game starts
+                break;
+        }
+    }
+
+    mouseClicked(): void {
+        if (this.clickCooldown > 0) return;
+
+        let buttonX = this.p.width / 2;
+
+        switch (this.MENU) {
+            case 0: // Main Menu
+                if (this.p.mouseX > buttonX - 100 && this.p.mouseX < buttonX + 100) {
+                    if (this.p.mouseY > this.p.height / 2 - 37 && this.p.mouseY < this.p.height / 2 + 37) {
+                        this.MENU = 1; // Play (goes to world select)
+                    } else if (this.p.mouseY > this.p.height / 2 + 63 && this.p.mouseY < this.p.height / 2 + 137) {
+                        this.MENU = 3; // Settings
+                    } else if (this.p.mouseY > this.p.height / 2 + 163 && this.p.mouseY < this.p.height / 2 + 237) {
+                        this.MENU = 4; // Customize
+                    }
+                }
+                break;
+
+            case 1: // World Select (all lead to demo world)
+                if (this.p.mouseX > buttonX - 100 && this.p.mouseX < buttonX + 100) {
+                    if (this.p.mouseY > this.p.height / 2 - 37 && this.p.mouseY < this.p.height / 2 + 37) {
+                        this.MENU = 2;
+                        this.WORLD = 0;
+                    } else if (this.p.mouseY > this.p.height / 2 + 63 && this.p.mouseY < this.p.height / 2 + 137) {
+                        this.MENU = 2;
+                        this.DIFFICULTY = 0;
+                    } else if (this.p.mouseY > this.p.height / 2 + 163 && this.p.mouseY < this.p.height / 2 + 237) {
+                        this.MENU = 2;
+                        this.DIFFICULTY = 0;
+                    }
+                } 
+                break;
+
+            case 2: // Difficulty Select
+                if (this.p.mouseX > buttonX - 100 && this.p.mouseX < buttonX + 100) {
+                    if (this.p.mouseY > this.p.height / 2 - 37 && this.p.mouseY < this.p.height / 2 + 37) {
+                        this.MENU = 99;
+                        this.DIFFICULTY = 0;
+                    } else if (this.p.mouseY > this.p.height / 2 + 63 && this.p.mouseY < this.p.height / 2 + 137) {
+                        this.MENU = 99;
+                        this.DIFFICULTY = 1;
+                    } else if (this.p.mouseY > this.p.height / 2 + 163 && this.p.mouseY < this.p.height / 2 + 237) {
+                        this.MENU = 99;
+                        this.DIFFICULTY = 2;
+                    }
+                }
+                break;
+        }
+
+        // Preventing one click from registering as multiple (this was a previous issue)
+        this.clickCooldown = 10;
+    }
+
+    // Return the current menu code
+    getMenuState(): number {
+        return this.MENU;
+    }
+
+    // Change the menu code
+    setMenuState(menu: number): void {
+        this.MENU = menu;
+    }
+
+    // Return the current difficulty
+    getDiff(): number {
+        return this.DIFFICULTY;
+    }
+    
+    // Return the selected world
+    getWorld(): number {
+        return this.WORLD;
+    }
+
+    private drawMainMenu(): void {
+        this.p.textFont('Courier New');
+
+        let buttonX = this.p.width / 2;
+        let buttonY = this.p.height / 2;
+        let buttonWidth = 200;
+        let buttonHeight = 75;
+
+        // Play Button
+        this.p.fill(0);
+        this.p.rect(buttonX, buttonY, buttonWidth, buttonHeight);
+        this.p.fill(255);
+        this.p.textSize(50);
+        this.p.textAlign(this.p.CENTER, this.p.CENTER);
+        this.p.text('PLAY', buttonX, buttonY);
+
+        // Settings Button
+        this.p.fill(0);
+        this.p.rect(buttonX, buttonY + 100, buttonWidth, buttonHeight);
+        this.p.fill(255);
+        this.p.textSize(26);
+        this.p.text('SETTINGS', buttonX, buttonY + 100);
+
+        // Customize Button
+        this.p.fill(0);
+        this.p.rect(buttonX, buttonY + 200, buttonWidth, buttonHeight);
+        this.p.fill(255);
+        this.p.textSize(26);
+        this.p.text('CUSTOMIZE', buttonX, buttonY + 200);
+    }
+
+    private drawWorldSelect(): void {
+        this.p.textFont('Courier New');
+
+        let buttonX = this.p.width / 2;
+        let buttonY = this.p.height / 2;
+        let buttonWidth = 200;
+        let buttonHeight = 75;
+
+        // World 1
+        this.p.fill(0);
+        this.p.rect(buttonX, buttonY, buttonWidth, buttonHeight);
+        this.p.fill(255);
+        this.p.textSize(50);
+        this.p.textAlign(this.p.CENTER, this.p.CENTER);
+        this.p.text('WORLD 1', buttonX, buttonY);
+
+        // World 2
+        this.p.fill(0);
+        this.p.rect(buttonX, buttonY + 100, buttonWidth, buttonHeight);
+        this.p.fill(255);
+        this.p.textSize(50);
+        this.p.text('WORLD 2', buttonX, buttonY + 100);
+
+        // World 3
+        this.p.fill(0);
+        this.p.rect(buttonX, buttonY + 200, buttonWidth, buttonHeight);
+        this.p.fill(255);
+        this.p.textSize(50);
+        this.p.text('WORLD 3', buttonX, buttonY + 200);
+    }
+
+    private drawDifficultySelect(): void {
+        this.p.textFont('Courier New');
+
+        let buttonX = this.p.width / 2;
+        let buttonY = this.p.height / 2;
+        let buttonWidth = 200;
+        let buttonHeight = 75;
+
+        // Easy
+        this.p.fill(0);
+        this.p.rect(buttonX, buttonY, buttonWidth, buttonHeight);
+        this.p.fill(255);
+        this.p.textSize(50);
+        this.p.text('EASY', buttonX, buttonY);
+
+        // Normal
+        this.p.fill(0);
+        this.p.rect(buttonX, buttonY + 100, buttonWidth, buttonHeight);
+        this.p.fill(255);
+        this.p.textSize(40);
+        this.p.text('NORMAL', buttonX, buttonY + 100);
+
+        // Hard
+        this.p.fill(0);
+        this.p.rect(buttonX, buttonY + 200, buttonWidth, buttonHeight);
+        this.p.fill(255);
+        this.p.textSize(50);
+        this.p.text('HARD', buttonX, buttonY + 200);
+    }
+}
--- src/sketch.ts
+++ src/sketch.ts
@@ -1,109 +1,44 @@
 import p5 from 'p5';
 import Player from './lib/Player';
+import Menu from './lib/MainMenu';
 
-let MENU = 0;
-let imgLogo: p5.Image;
 const player = new Player();
+let menu: Menu;
 
 const sketch = (p: p5) => {
     p.preload = () => {
-        imgLogo = p.loadImage('/logo.png');
+        menu = new Menu(p);
+        menu.preload();
     };
 
     p.setup = () => {
         p.createCanvas(window.innerWidth, window.innerHeight);
-        p.rectMode(p.CENTER);
+        menu.setup();
         player.setup(p);
     };
 
     p.keyPressed = (e: KeyboardEvent) => {
-        if (e.key === "Escape") {
-            MENU = 0;
+        if (e.key === "Escape") { // When ESC is pressed...
+            menu.setMenuState(0); // ...return to main menu
         }
         player.keyPressed(e);
     };
 
     p.keyReleased = (e: KeyboardEvent) => {
-        if (e.key === "Escape") {
-            MENU = 0;
-        }
         player.keyReleased(e);
     };
 
     p.draw = () => {
-        p.background(imgLogo);
+        menu.draw(); // Draw the main meun
 
-        if (MENU === 0) {
-            drawMainMenu(p);
-        } else if (MENU === 1) {
-            p.background(0, 255, 0);
-            p.fill(0);
-            p.textSize(20);
-            p.text('Difficulty settings go here', p.width / 2, 30);
-        } else if (MENU === 2) {
-            p.background(255, 0, 255);
-            p.textSize(30);
-            p.fill(0);
-            p.text('Settings', p.width / 2, 150);
-        } else if (MENU === 3) {
-            p.background(255, 0, 0);
-            p.textSize(50);
-            p.fill(255);
-            p.text('Character Customization', p.width / 2, p.height / 2);
-        }
-
-        if (MENU !== 0) {
+        if (menu.getMenuState() === 99) { // 99 means user isn't in a menu (see MainMenu.ts comments)
             player.draw(p);
         }
     };
 
     p.mouseClicked = () => {
-        if (MENU === 0) {
-            let buttonX = p.width / 2;
-
-            if (p.mouseX > buttonX - 100 && p.mouseX < buttonX + 100) {
-                if (p.mouseY > p.height / 2 - 37 && p.mouseY < p.height / 2 + 37) {
-                    MENU = 1; // Play
-                } else if (p.mouseY > p.height / 2 + 63 && p.mouseY < p.height / 2 + 137) {
-                    MENU = 2; // Settings
-                } else if (p.mouseY > p.height / 2 + 163 && p.mouseY < p.height / 2 + 237) {
-                    MENU = 3; // Customize
-                }
-            }
-        }
+        menu.mouseClicked();
     };
 };
-
-function drawMainMenu(p: p5) {
-    p.textFont('Courier New');
-
-    // Button dimensions
-    let buttonWidth = 200;
-    let buttonHeight = 75;
-    let buttonX = p.width / 2;
-    let buttonY = p.height / 2;
-
-    // Play Button
-    p.fill(0);
-    p.rect(buttonX, buttonY, buttonWidth, buttonHeight);
-    p.fill(255);
-    p.textSize(50);
-    p.textAlign(p.CENTER, p.CENTER);
-    p.text('PLAY', buttonX, buttonY);
-
-    // Settings Button
-    p.fill(0);
-    p.rect(buttonX, buttonY + 100, buttonWidth, buttonHeight);
-    p.fill(255);
-    p.textSize(26);
-    p.text('SETTINGS', buttonX, buttonY + 100);
-
-    // Customize Button
-    p.fill(0);
-    p.rect(buttonX, buttonY + 200, buttonWidth, buttonHeight);
-    p.fill(255);
-    p.textSize(26);
-    p.text('CUSTOMIZE', buttonX, buttonY + 200);
-}
 
 const game = new p5(sketch, document.getElementById('p5-canvas') as HTMLElement);
