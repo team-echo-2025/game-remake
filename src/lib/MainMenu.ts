d0ef4a4a628ae8cd296c73fc26c175446d087e11
24Fan <120273527+24Fan@users.noreply.github.com>
2025-02-01 23:57:09 -0500

    Changed buttons and implemented audio/mute.

--- src/lib/MainMenu.ts
+++ src/lib/MainMenu.ts
@@ -1,10 +1,29 @@
 import p5 from 'p5';
 import GameObject from './GameObject';
 
+/*
+Menu Codes:
+0 = Main menu
+1 = World Select
+2 = Difficulty
+3 = Settings
+4 = Customization
+6 = Keybinds
+99 = Game (no longer in a menu)
+*/
+
+/*
+World Codes:
+0 = Demo
+1 = World 1
+2 = World 2
+3 = World 3
+*/
+
 export default class Menu implements GameObject {
     private imgBackground!: p5.Image;
+    private imgButton!: p5.Image;
     private font!: p5.Font;
-    private clickCooldown = 0;
     private mute = false;
 
     private menuState = 0;
@@ -16,6 +35,7 @@ export default class Menu implements GameObject {
     preload(): void {
         this.font = this.p.loadFont('assets/fonts/jersey.ttf');
         this.imgBackground = this.p.loadImage('/assets/background.png');
+        this.imgButton = this.p.loadImage('/assets/Button_Flesh.png');
     }
 
     setup(): void {
@@ -24,8 +44,6 @@ export default class Menu implements GameObject {
     }
 
     draw(): void {
-        if (this.clickCooldown > 0) this.clickCooldown--;
-
         this.p.background(this.imgBackground);
         switch (this.menuState) {
             case 0: this.drawMainMenu(); break;
@@ -38,12 +56,9 @@ export default class Menu implements GameObject {
     }
 
     mouseClicked(): void {
-        if (this.clickCooldown > 0) return;
 
         let buttonX = this.p.width / 2;
         let buttonY = this.p.height / 2;
-        let buttonWidth = 200;
-        let buttonHeight = 75;
 
         const buttonClicked = (yOffset: number) => 
             this.p.mouseX > buttonX - 100 && this.p.mouseX < buttonX + 100 &&
@@ -57,6 +72,8 @@ export default class Menu implements GameObject {
                 break;
             case 1: // World select
                 if (buttonClicked(0)) { this.menuState = 2; this.world = 0; }
+                else if (buttonClicked(100)) { this.menuState = 2; this.world = 0; }
+                else if (buttonClicked(200)) { this.menuState = 2; this.world = 0; }
                 break;
             case 2: // Difficulty select
                 if (buttonClicked(0)) { this.menuState = 99; this.difficulty = 0; }
@@ -64,39 +81,53 @@ export default class Menu implements GameObject {
                 else if (buttonClicked(200)) { this.menuState = 99; this.difficulty = 2; }
                 break;
             case 3: // Settings
-                if (buttonClicked(100)) this.menuState = 6;
+                if (buttonClicked(0)) this.handleMute();
+                else if (buttonClicked(100)) this.menuState = 6;
                 else if (buttonClicked(200)) this.menuState = 0;
                 break;
         }
-
-        this.clickCooldown = 10;
     }
 
     private drawMainMenu(): void {
+        this.p.textFont(this.font);
+        this.p.fill(255);
+        this.p.textSize(200);
+        this.p.text('Exit Paradox', this.p.width / 2, (this.p.height / 2) - 200);
         this.drawButton('PLAY', 0);
         this.drawButton('SETTINGS', 100);
         this.drawButton('CUSTOMIZE', 200);
     }
 
     private drawWorldSelect(): void {
+        this.p.fill(255);
+        this.p.textSize(200);
+        this.p.text('Exit Paradox', this.p.width / 2, (this.p.height / 2) - 200);
         this.drawButton('WORLD 1', 0);
+        this.drawButton('WORLD 2', 100);
+        this.drawButton('WORLD 3', 200);
     }
 
     private drawDifficultySelect(): void {
+        this.p.fill(255);
+        this.p.textSize(200);
+        this.p.text('Exit Paradox', this.p.width / 2, (this.p.height / 2) - 200);
         this.drawButton('EASY', 0);
         this.drawButton('NORMAL', 100);
         this.drawButton('HARD', 200);
     }
 
     private drawSettingsMenu(): void {
+        this.p.fill(255);
+        this.p.textSize(200);
+        this.p.text('Exit Paradox', this.p.width / 2, (this.p.height / 2) - 200);
         this.drawButton(`MUTE: ${this.mute ? 'ON' : 'OFF'}`, 0);
         this.drawButton('KEYBINDS', 100);
         this.drawButton('BACK', 200);
     }
 
     private drawCustomization(): void {
-        this.p.background(255, 0, 0);
-        this.p.fill(255);
+        this.p.background(135, 206, 235);
+        this.p.fill(0);
         this.p.textSize(50);
         this.p.text('Character Customization', this.p.width / 2, this.p.height / 2);
     }
@@ -114,16 +145,26 @@ export default class Menu implements GameObject {
         let buttonWidth = 200;
         let buttonHeight = 75;
 
-        this.p.fill(0);
-        this.p.rect(buttonX, buttonY, buttonWidth, buttonHeight);
+        this.p.image(this.imgButton, (this.p.width / 2) - 100, this.p.height / 2 + yOffset - 75 / 2 + 10, 200, 75);
         this.p.fill(255);
-        this.p.textSize(40);
+        this.p.textSize(25);
         this.p.textAlign(this.p.CENTER, this.p.CENTER);
         this.p.text(label, buttonX, buttonY);
     }
 
     private toggleMute(e: KeyboardEvent): void {
-        if (e.key.toLowerCase() === 'm') this.mute = !this.mute;
+        if (e.key.toLowerCase() === 'm') {
+            this.handleMute();
+        }
+    }
+
+    private handleMute() {
+        this.mute = !this.mute;
+            document.dispatchEvent(new CustomEvent("onmute", {
+                detail: {
+                    mute: this.mute
+                }
+            }))
     }
 
     getMenuState(): number { return this.menuState; }
--- src/sketch.ts
+++ src/sketch.ts
@@ -1,11 +1,10 @@
 import p5 from 'p5';
 import Player from './lib/Player';
 import Menu from './lib/MainMenu';
-// import SettingsMenu from './lib/SettingsMenu';
+import {Howl} from 'howler';
 
 let menu: Menu;
 let player: Player;
-// let setting: SettingsMenu;
 
 
 const sketch = (p: p5) => {
@@ -14,21 +13,29 @@ const sketch = (p: p5) => {
         player = new Player(p);
         menu.preload();
         player.preload();
-        // setting = new SettingsMenu(p);
-        // setting.preload();
     };
 
     p.setup = () => {
+        const sound = new Howl({
+            src: "assets/background_music.mp3"
+        })
+        sound.play();
+        document.addEventListener("onmute", (e: any)=>{
+            console.log(e.detail)
+            if (e.detail.mute) {
+                sound.pause();
+            } else {
+                sound.play();
+            }
+        });
         p.createCanvas(window.innerWidth, window.innerHeight);
         menu.setup();
         player.setup();
-        // setting.setup();
     };
 
     p.keyPressed = (e: KeyboardEvent) => {
         if (e.key === "Escape") { // When ESC is pressed...
             menu.setMenuState(0); // ...return to main menu
-            // setting.setMenuState(0);
         }
         player.keyPressed(e);
     };
@@ -40,7 +47,6 @@ const sketch = (p: p5) => {
     p.draw = () => {
         p.background(135, 206, 235);
         menu.draw(); // Draw the main menu
-        // setting.draw();
 
         if (menu.getMenuState() === 99) { // 99 means user isn't in a menu (see MainMenu.ts comments)
             player.draw();
@@ -49,7 +55,6 @@ const sketch = (p: p5) => {
 
     p.mouseClicked = () => {
         menu.mouseClicked();
-        // setting.mouseClicked();
     };
 };
 
