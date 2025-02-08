7554f9633e7a0735b8674fbb5e37021eb37c99ef
24Fan <120273527+24Fan@users.noreply.github.com>
2025-02-08 13:12:21 -0500

    Fixed fonts, added back buttons, corrected grammar

--- src/scenes/DifficultySelectScene.ts
+++ src/scenes/DifficultySelectScene.ts
@@ -2,39 +2,44 @@ import Scene from "../lib/Scene";
 import Button from "../lib/ui/Button";
 
 export default class DifficultyScene extends Scene {
-    world1: Button;
-    world2: Button;
-    world3: Button;
+    easy: Button;
+    normal: Button;
+    hard: Button;
+    buttonBack: Button;
     constructor() {
         super("difficulty-scene");
-        this.world1 = new Button({
+        this.easy = new Button({
             label: "EASY",
             scene: this,
-            font_size: 50,
             callback: () => { this.start("play-scene") }
         })
-        this.world2 = new Button({
+        this.normal = new Button({
             label: "NORMAL",
             scene: this,
-            font_size: 50,
             callback: () => { this.start("play-scene") }
         })
-        this.world3 = new Button({
+        this.hard = new Button({
             label: "HARD",
             scene: this,
-            font_size: 50,
             callback: () => { this.start("play-scene") }
+        })
+        this.buttonBack = new Button({
+            label: "Back",
+            scene: this,
+            callback: () => { this.start("world-scene") }
         })
     }
 
     onStart(): void {
-        this.add(this.world1);
-        this.add(this.world2);
-        this.add(this.world3);
+        this.add(this.easy);
+        this.add(this.normal);
+        this.add(this.hard);
+        this.add(this.buttonBack);
     }
 
     draw() {
-        this.world1.y = -100;
-        this.world3.y = 100;
+        this.buttonBack.y = 200;
+        this.easy.y = -100;
+        this.hard.y = 100;
     }
 }
--- src/scenes/WorldSelectScene.ts
+++ src/scenes/WorldSelectScene.ts
@@ -5,25 +5,28 @@ export default class WorldScene extends Scene {
     world1: Button;
     world2: Button;
     world3: Button;
+    buttonBack: Button;
     constructor() {
         super("world-scene");
         this.world1 = new Button({
             label: "WORLD 1",
             scene: this,
-            font_size: 50,
             callback: () => { this.start("difficulty-scene") }
         })
         this.world2 = new Button({
             label: "WORLD 2",
             scene: this,
-            font_size: 50,
             callback: () => { this.start("difficulty-scene") }
         })
         this.world3 = new Button({
             label: "WORLD 3",
             scene: this,
-            font_size: 50,
             callback: () => { this.start("difficulty-scene") }
+        })
+        this.buttonBack = new Button({
+            label: "Back",
+            scene: this,
+            callback: () => { this.start("menu-scene") }
         })
     }
 
@@ -31,10 +34,12 @@ export default class WorldScene extends Scene {
         this.add(this.world1);
         this.add(this.world2);
         this.add(this.world3);
+        this.add(this.buttonBack);
     }
 
     draw() {
         this.world1.y = -100;
         this.world3.y = 100;
+        this.buttonBack.y = 200;
     }
 }
