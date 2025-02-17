4bdfa04def79924f86c3e750d4ca1f10e4403a4f
Andrew Leonard <andrew@Andrew-Laptop>
2025-02-17 15:37:02 -0500

    physics scene

---
+++ src/scenes/PhysicsTestScene.ts
@@ -0,0 +1,39 @@
+import Player from "../lib/Player";
+import Scene from "../lib/Scene";
+import PhysicsObject from "../lib/PhysicsObject";
+
+export default class PhysicsTestScene extends Scene {
+    player?: Player;
+    physicalObject?: PhysicsObject;
+
+    constructor() {
+        super("physics-scene");
+    }
+
+    onStart(): void {
+        this.player = new Player(this);
+        this.add(this.player);
+    }
+
+    setup() : void
+    {
+        this.physicalObject = this.add_new.physics_object
+        ({
+            x: 200,
+            y: 0,
+            width: 100,
+            height: 100,
+            mass: 1,
+            scene: this
+        })
+    }
+
+    preload(): any {
+        console.log("begin preload of Physics scene");
+    }
+
+    onStop()
+    {
+        this.player = undefined;
+    }
+}
