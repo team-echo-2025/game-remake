b7d58912f1ceedcce1a8806441d048fd46dc8f30
Peter Nguyen <SSBTdrag999@gmail.com>
2025-03-01 14:42:22 -0500

    start

--- src/puzzles/GraveyardSwitches/GraveyardSwitches.ts
+++ src/puzzles/GraveyardSwitches/GraveyardSwitches.ts
@@ -1,2 +1,12 @@
 import Puzzle, { PuzzleState } from "../../lib/Puzzle";
+import PhysicsObject from "../../lib/physics/PhysicsObject";
+import RigidBody from "../../lib/physics/RigidBody";
+import Player from "../../lib/Player";
 
+
+import Scene from "../../lib/Scene";
+
+
+export default class GraveyardSwitches extends Puzzle {
+    
+}
