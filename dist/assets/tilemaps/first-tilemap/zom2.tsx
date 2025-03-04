f3d9a3def4d29ddf63bb52065c02763b8fb9c9e8
nboj <60493251+nboj@users.noreply.github.com>
2025-03-03 19:56:09 -0500

    added build

---
+++ dist/assets/tilemaps/first-tilemap/zom2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="zom2" tilewidth="48" tileheight="48" tilecount="144" columns="24">
+ <image source="tilesets/headless_zombie.png" width="1152" height="288"/>
+</tileset>
