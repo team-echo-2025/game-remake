61d1685c26eb97492a87040126ca7fb645b4f907
nboj <60493251+nboj@users.noreply.github.com>
2025-03-05 17:30:18 -0500

    Revert "added build"
    This reverts commit f3d9a3def4d29ddf63bb52065c02763b8fb9c9e8.

--- dist/assets/tilemaps/PetersTileMap/1st floor.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="1st floor" tilewidth="32" tileheight="32" tilecount="260" columns="20">
- <image source="32 Backgrounds Free!/32 Backgrounds Free!/Horizontal/29.png" width="640" height="435"/>
-</tileset>
--- dist/assets/tilemaps/PetersTileMap/farmland.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="farmland" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="Farm Game Dungeon Cellar Tileset/Tilesets/FG_Cellar.png" width="512" height="512"/>
-</tileset>
