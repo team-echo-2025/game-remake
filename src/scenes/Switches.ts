3f566223cac148445023981c96a41960b8fe5daa
Peter Nguyen <SSBTdrag999@gmail.com>
2025-04-07 18:34:09 -0400

    broken

--- src/scenes/Switches.ts
+++ src/scenes/Switches.ts
@@ -22,16 +22,14 @@ type SceneState = {
 }
 
 export default class Switches extends Scene {
-    x: number = 0; //position of the stones
-    y: number = 0;
     pManager: PageManager;
     player?: Player;
     tilemap?: Tilemap;
-    firstSwitch: [number, number] | null;
-    secondSwitch: [number, number] | null;
-    foundSwitch: boolean;
+    firstSwitch?: [number, number];
+    secondSwitch?: [number, number];
+    foundFirst: boolean;
     positions: [number, number][] = [];
-    switchState: [] = [];
+    switches: PhysicsObject[] =[];
     graveSwitch!: PhysicsObject;
     dialogue!: Dialogue;
     inRange: boolean = false;
@@ -42,9 +40,7 @@ export default class Switches extends Scene {
         super("Switches");
         this.physics.debug = false;
         this.pManager = new PageManager([new SwitchesPage()], this)
-        this.firstSwitch = null;
-        this.secondSwitch = null;
-        this.foundSwitch = false;
+        this.foundFirst = false;
 
         this.camera.zoom = 3;
     }
@@ -61,73 +57,104 @@ export default class Switches extends Scene {
         this.player.body.y = -70;
         this.physics.addObject(this.player);
 
-        this.switchState.push();
     }
     preload(): any {
         this.loadFont("jersey", "assets/fonts/jersey.ttf");
         this.loadTilemap("tilemap", "assets/tilemaps/PetersTileMap/Switches.tmx");
-        this.loadImage("switchesOff", "assets/tilemaps/PetersTileMaps/switchesOff.png");
+        this.loadImage("switchesOff", "assets/tilemaps/PetersTileMap/switchesOff.png");
         this.loadImage("switchesOn", "assets/tilemaps/PetersTilemap/switchesOn.png");
     }
     setup(): void {
         // Boundaries of the grid
         this.tilemap = this.add_new.tilemap({ tilemap_key: "tilemap" });
-        const gridSize = new PhysicsObject({
-            width: 550,
-            height: 220,
-            mass: Infinity
-        })
-        gridSize.body.x = -82;
-        gridSize.body.y = -190; //(350,-295), (188,-295), (188,-95),(350,-95) corners
-        gridSize.overlaps = true;
-        this.physics.addObject(gridSize);
-        this.graveSwitch = new PhysicsObject({
-            width: 50,
-            height: 50,
-            mass: Infinity
-        });   
-        this.graveSwitch.overlaps = true;
-        this.graveSwitch.body.x = this.x;
-        this.graveSwitch.body.y = this.y;
-        this.physics.addObject(this.graveSwitch);
 
-        this.dialogue = new Dialogue(this, this.player!);
-        
         this.positions = [ // graveyard positions
             [-302,-257], [-238,-257], [-175, -257], [-111, -257], [-47, -257], [17, -257], [81, -257], [145, -257], 
             [-302, -190], [-238,-190], [-175, -190], [-111, -190], [-47, -190], [17, -190], [81, -190], [145, -190],   
             [-302, -125], [-238,-125], [-175, -125], [-111, -125], [-47, -125], [17, -125], [81, -125], [145, -125]
         ]
 
-        this.graveSwitch.onCollide = (other: RigidBody) => {
-            if(other == this.player?.body){
-                clearTimeout(this.collider_timeout);
-                if(!this.inRange && this.foundSwitch){
-                    this.inRange = true;
-                    this.asset.change_asset("switchesOn");
-                }
-                this.collider_timeout = setTimeout(() => {
-                    this.inRange = false;
-                    this.asset.change_asset("switchesOff");
-                }, 100);
-            }
-        }
-
-
-
-
         const shuffled = [...this.positions].sort(()=> Math.random() - 0.5);
         this.firstSwitch = shuffled[0]; //first position should be random
-        this.secondSwitch = shuffled[1]; // second position should be 1 up, down, left, and right of it.
+        this.secondSwitch = this.findAdjacent(shuffled[0]); // second position should be 1 up, down, left, and right of it.
+
+        for(const pos of this.positions){
+
+            const switchObj = new PhysicsObject({
+            width: 50,
+            height: 50,
+            mass: Infinity
+            });   
+            switchObj.body.x = pos[0];
+            switchObj.body.y = pos[1];
+            switchObj.overlaps = true;
+            this.physics.addObject(switchObj);
+            this.switches.push(switchObj);
+
+            switchObj.onCollide = (other: RigidBody) => {
+                if (other === this.player?.body) {
+                    this.handleSwitchPress(pos);
+                }
+            };
+
+        
+        }
+        this.dialogue = new Dialogue(this, this.player!);
+        // this.graveSwitch.onCollide = (other: RigidBody) => {
+        //     if(other == this.player?.body){
+        //         clearTimeout(this.collider_timeout);
+        //         if(!this.inRange && this.foundFirst){
+        //             this.inRange = true;
+        //             this.asset.change_asset("switchesOn");
+        //         }
+        //         this.collider_timeout = setTimeout(() => {
+        //             this.inRange = false;
+        //             this.asset.change_asset("switchesOff");
+        //         }, 100);
+        //     }
+        // }
         
 
 
     }
 
+    findAdjacent(pos: [number, number]): [number, number] | undefined {
+        const [x, y ] = pos;
+        const possible = [
+            [x + 64, y], [x - 64, y], [x, y + 64], [x, y - 64]
+        ];
+        for(const p of possible) {
+            if (this.positions.find(q => q[0] === p[0] && q[1] === p[1])) {
+                return [p[0], p[1]];
+            }
+        }
+        return undefined;
+        
+    }
+    handleSwitchPress(pos: [number,number]){
+        if(!this.foundFirst) {
+            if(this.firstSwitch && pos[0] === this.firstSwitch[0] && pos[1] === this.firstSwitch[1]) {
+                this.foundFirst = true;
+                this.dialogue.addDialogue(110, 50, "You found the first switch! Find the second one!");
+            } 
+            else {
+                this.dialogue.addDialogue(110, 50, "Wrong switch! Try again.");
+            }
+        }
+        else {
+            if (this.secondSwitch && pos[0] === this.secondSwitch[0] && pos[1] === this.secondSwitch[1]) {
+                this.dialogue.addDialogue(110, 50, "Both switches activated! Puzzle complete!");
+                // move the player somewhere like scene 5
+            } else {
+                this.dialogue.addDialogue(110, 50, "Wrong second switch! Start over.");
+                this.foundFirst = false;
+            }
+        }
+    }
     keyPressed = (e: KeyboardEvent) => {
 
         if(e.key === 'e') {              
-            if(this.foundSwitch){
+            if(this.foundFirst){
                 this.dialogue.addDialogue(110,50,"Hmm, there is nothing here");
                 
             }
