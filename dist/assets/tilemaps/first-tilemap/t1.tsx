87e4ae34b725d8bac95c9ad0afc0a624751caca2
nboj <60493251+nboj@users.noreply.github.com>
2025-04-30 18:54:55 -0400

    build

---
+++ dist/assets/tilemaps/first-tilemap/t1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="t1" tilewidth="48" tileheight="48" tilecount="8588" columns="76">
+ <image source="tilesets/room_buider.png" width="3648" height="5424"/>
+</tileset>
