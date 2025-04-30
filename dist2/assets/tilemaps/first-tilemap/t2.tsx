a5b04ea8e8a11231f8494fc3235f7d72543e21cb
nboj <60493251+nboj@users.noreply.github.com>
2025-04-30 19:07:42 -0400

    build

---
+++ dist2/assets/tilemaps/first-tilemap/t2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="t2" tilewidth="48" tileheight="48" tilecount="13744" columns="16">
+ <image source="tilesets/interior.png" width="768" height="41232"/>
+</tileset>
